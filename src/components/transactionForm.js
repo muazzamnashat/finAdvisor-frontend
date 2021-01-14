0975e99b7394d17605a07e3681476fa0c18a3fc7
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-01-14 12:50:15 -0500

    add react hook useState in form

--- src/components/transactionForm.js
+++ src/components/transactionForm.js
@@ -1,83 +1,95 @@
-import React from "react";
+import React, { useState } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import {connect} from "react-redux"
 
-const currencies = [
-  {
-    value: "USD",
-    label: "$"
-  },
-  {
-    value: "EUR",
-    label: "€"
-  },
-  {
-    value: "BTC",
-    label: "฿"
-  },
-  {
-    value: "JPY",
-    label: "¥"
-  }
-];
+function TransactionForm ({categories}){
+const [date,setDate] = useState("")
+const [description,setDescription] = useState("")
+const [category,setCategory] = useState("")
+const [amount,setAmount] = useState(0)
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: "25ch"
-    },
-   button: {
+    root: {
+      "& .MuiTextField-root": {
         margin: theme.spacing(1),
-    },
+        width: "25ch"
+      },
+     button: {
+          margin: theme.spacing(1),
+      },
+    }
+  }));
+const classes = useStyles();
+const handleChange = event => {
+    switch (event.target.name) {
+        case "date":
+            setDate(event.target.value)
+            break;
+        case "description":
+            setDescription(event.target.value)
+            break;
+        case "category":
+            setCategory(event.target.value)
+            break;
+        case "amount":
+            setAmount(event.target.value)
+            break;
+        default:
+            break;
+    }
   }
-}));
+ 
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+          <div>
+            <label>Date</label>
+            <TextField type="date" name="date" onChange={handleChange}/>
+    
+            <label>Description</label>
+            <TextField type="text" name="description" onChange={handleChange}/>
+            <label>Amount</label>
+            <TextField type="number" name="amount" onChange={handleChange}/>
+            
+            <label>Category</label>
+            <TextField
+              select
+            //   value={currency}
+              onChange={handleChange}
+              helperText="Please select a category"
+            >
+              {categories.map((category,index) => (
+                <MenuItem key={index} value={category}>
+                  {category}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Create Transaction
+          </Button>
+        </form>
+      );
 
-export default function TransactionForm() {
-  const classes = useStyles();
-  const [currency, setCurrency] = React.useState("EUR");
-
-  const handleChange = (event) => {
-    setCurrency(event.target.value);
-  };
-
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <label>Date</label>
-        <TextField type="date" />
-
-        <label>Description</label>
-        <TextField type="text" />
-        <label>Amount</label>
-        <TextField type="number" />
-        
-        <label>Category</label>
-        <TextField
-          select
-          value={currency}
-          onChange={handleChange}
-          helperText="Please select a category"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-      <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Create Transaction
-      </Button>
-    </form>
-  );
+  
 }
+
+
+
+const mapStateToProps = state => {
+    return {
+      categories: state.categories
+    }
+}
+   
+  
+export default connect(mapStateToProps)(TransactionForm)
