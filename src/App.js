135f94e0a0762595d07c425a9d26a1372482eb05
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-01-15 11:51:07 -0500

    add signup form

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import React, { Component } from 'react'
-import {API} from './api/API'
-import Transactions from './containers/transactions'
-import CenteredTabs from './components/navMenu'
-import Dashboard from './components/Dashboard'
-import { connect } from 'react-redux'
-import {fetchTransactions} from './actions/fetchTransactions'
-import {fetchCategories} from './actions/fetchCategories'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import { API } from "./api/API";
+import Transactions from "./containers/transactions";
+import CenteredTabs from "./components/navMenu";
+import Dashboard from "./components/Dashboard";
+import { connect } from "react-redux";
+import { fetchTransactions } from "./actions/fetchTransactions";
+import { fetchCategories } from "./actions/fetchCategories";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignUp from "./components/signup";
 
 class App extends Component {
-
   componentDidMount() {
-    this.props.fetchTransactions()
-    this.props.fetchCategories()
+    // this.props.fetchTransactions();
+    // this.props.fetchCategories();
     // console.log(this.props.transactions)
   }
   render() {
-    return(
+    return (
       <Router>
         <div>
-          <Route path="/" render={routerProps => <Dashboard {...routerProps} transactions={this.props.transactions}/>} />
-          
+          <Route exact path="/signup" render={(routerProps) => <SignUp />} />
+          <Route
+            exact
+            path="/"
+            render={(routerProps) => (
+              <Dashboard
+                {...routerProps}
+                transactions={this.props.transactions}
+              />
+            )}
+          />
+
           {/* <Transactions/> */}
         </div>
       </Router>
-    )
+    );
   }
-    
 }
 
-
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    transactions: state.transactions
-  }
-}
- 
-const mapDispatchToProps = dispatch => {
+    transactions: state.transactions,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
   return {
     fetchTransactions: () => dispatch(fetchTransactions()),
-    fetchCategories: () => dispatch(fetchCategories())
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App)
-
+    fetchCategories: () => dispatch(fetchCategories()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
